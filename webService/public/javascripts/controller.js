62121e3576f5e088e192c625be38533b6ab55c33
Tania <taniapapazaf@gmail.com>
2015-08-24 18:09:50 +0100

    Fixed days with single digit issues
    Tasks for days with single digits where not showing. I fixed that by
    adding a leading zero, and removing when printing the calendar

--- webService/public/javascripts/controller.js
+++ webService/public/javascripts/controller.js
@@ -8,6 +8,10 @@ calendarController.controller('monthCtrl', function($scope, $http, $filter, $loc
 	
     var now = new Date();
     
+	$scope.twoDigit = function(num) {
+		return ('0' + num).slice(-2);
+	}
+	
     $scope.getNumber = function(num) {
         return new Array(num);   
     }
@@ -16,12 +20,12 @@ calendarController.controller('monthCtrl', function($scope, $http, $filter, $loc
     $scope.totalDays = parseInt(daysInMonth, 10);
     $scope.nameOfMonth = now.toLocaleString('en-us', { month: 'long' });
     //$scope.date = now.getFullYear() + '-' + (now.getMonth()+1) + '-' + now.getDate();
-    $scope.yearMonth = now.getFullYear() + '-' + ('0' + (1+now.getMonth())).slice(-2) + '-';
+    $scope.yearMonth = now.getFullYear() + '-' + $scope.twoDigit(1+now.getMonth()) + '-';
 	
 });
 
 calendarController.controller('dayCtrl', function($scope, $http, $filter, $location) {
-    $scope.day = $scope.date.split('-').slice(-1)[0]; // Get the day from the date
+    $scope.day = $scope.date.split('-').slice(-1)[0].replace(/\b0(?=\d)/g, ''); // Get the day from the date and remove leading 0s
     
     $http.get('/tasks/date/' + $scope.date).
     success(function(data, status, headers, config) {
@@ -41,3 +45,4 @@ calendarFilters.filter('num', function() {
       return parseInt(input, 10);
     };
 });
+
