7d22a9c4cac44f5c9d10e10341a67fbe76efc204
Tania <taniapapazaf@gmail.com>
2016-01-09 21:20:50 +0000

    Updated the favicon

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname + '/public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
